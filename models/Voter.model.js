f70899a8c879539162cd885e2422656714ef9e08
Anna Kijania <kijania.anna@gmail.com>
2020-12-21 21:27:23 +0100

    Change type of votes in Voter model

--- models/Voter.model.js
+++ models/Voter.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const voterSchema = new mongoose.Schema({
   user: { type: String, required: true },
-  votes: { type: Number, required: true },
+  votes: { type: Array, required: true },
 });
 
 module.exports = mongoose.model('Voter', voterSchema);
